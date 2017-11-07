e852da64fd5d56b5a9cdad314ad1b04358d5b9dc
Collin Banks <jazztbone2b@gmail.com>
2017-11-06 20:53:06 -0500

    added a fade in animation on scrolldown

--- Portfolio.js
+++ Portfolio.js
@@ -110,7 +110,7 @@ TxtType.prototype.tick = function() {
     }, delta);
 };
 
-window.onload = function() {
+function typeContent() {
     var elements = document.getElementsByClassName('typewrite');
     for (var i=0; i<elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
@@ -125,3 +125,28 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+function fadeContent() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.fade-in').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+            }
+            
+        }); 
+    
+    });
+    
+};
+
+window.onload = typeContent(), fadeContent();
